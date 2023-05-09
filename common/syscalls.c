e3c96279d4687cfcebdb317a79ea9452d7a70797
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2023-05-09 11:27:12 +0100

    Fix linking under gcc 10.2.0.

--- common/syscalls.c
+++ common/syscalls.c
@@ -5,6 +5,7 @@ extern int uart_tx_one_char(uint8_t c);
 
 ssize_t _write(int fildes, const void *buf, size_t nbyte)
 {
+    (void)fildes;
     const uint8_t *cbuf = (const uint8_t *) buf;
     for (size_t i = 0; i < nbyte; ++i) {
         uart_tx_one_char(cbuf[i]);
